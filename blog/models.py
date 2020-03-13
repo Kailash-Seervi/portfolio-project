9267a8533caa5f6dfccb3203966bc9c55906607b
Kailash_R <kseervi257@gmail.com>
2020-03-13 11:05:40 +0530

    Added bootstraps and jobs are showing up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+# Create A blog
+# title
+# pub_date
+# body
+# image
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image=models.ImageField(upload_to='images/')
